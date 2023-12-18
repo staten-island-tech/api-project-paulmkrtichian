e12ebfd9230ab3ed9b2b3f7b1036f585d9662a76
paulmkrtichian <paulmkr22@gmail.com>
2023-12-18 09:21:32 -0500

    12/18

--- api-project/main.js
+++ api-project/main.js
@@ -9,12 +9,15 @@ async function getData(URL) {
     console.log(data.data.results);
   } catch (error) {
     console.error(error);
-    // Handle the error, for example, update an element with an error message:
-    // document.querySelector("h1").textContent = error.message;
+  
   }
 }
 
 getData(URL);
+
+
+
+
 
 
 /*https://gateway.marvel.com:443/v1/public/characters?apikey=09a4303b1ef760690190a1ab65d7eafc&hash=*/
@@ -23,3 +26,11 @@ getData(URL);
 //PUBLIC: 09a4303b1ef760690190a1ab65d7eafc
 //Private: 02fdb37face372fee7c6e1cf85f2f2d451c89c3a
 //Hash: 3a1cadafb0cb19882d6df0a72dda0f43
+
+
+
+
+
+
+
+
