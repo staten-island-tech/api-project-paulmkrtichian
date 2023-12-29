3638ede722ed5ea3709f3827197186888ffca2ab
paulmkrtichian <paulmkr22@gmail.com>
2023-12-29 15:56:56 -0500

    12/29

--- api-project/main.js
+++ api-project/main.js
@@ -1,4 +1,4 @@
-const URL = `https://gateway.marvel.com/v1/public/comics?ts=1&apikey=09a4303b1ef760690190a1ab65d7eafc&hash=3a1cadafb0cb19882d6df0a72dda0f43&limit=100`;
+/*  const URL = `https://gateway.marvel.com/v1/public/comics?ts=1&apikey=09a4303b1ef760690190a1ab65d7eafc&hash=3a1cadafb0cb19882d6df0a72dda0f43&limit=100`;
 
 async function getData(URL) {
   try {
@@ -13,16 +13,16 @@ async function getData(URL) {
   }
 }
 
- getData(URL);
+ getData(URL); */
 
- function displayresults(data) {
-   data.forEach((marvel) => {
+/* function displayresults(data) {
+  data.forEach((marvel) => {
     const marveltitles = marvel.title.map((comic) => comic.title);
     console.log(marveltitles)
 
 
-   })
- }
+  })
+} 
 
 
 /*https://gateway.marvel.com:443/v1/public/characters?apikey=09a4303b1ef760690190a1ab65d7eafc&hash=*/
@@ -31,11 +31,67 @@ async function getData(URL) {
 //PUBLIC: 09a4303b1ef760690190a1ab65d7eafc
 //Private: 02fdb37face372fee7c6e1cf85f2f2d451c89c3a
 //Hash: 3a1cadafb0cb19882d6df0a72dda0f43
+ 
 
+const URL = `https://gateway.marvel.com/v1/public/comics?ts=1&apikey=09a4303b1ef760690190a1ab65d7eafc&hash=3a1cadafb0cb19882d6df0a72dda0f43&limit=100`;
 
+document.getElementById('search').addEventListener('click', () => {
+  const searchTerm = document.getElementById('comicTitle').value;
+  searchComicByTitle(URL, searchTerm);
+});
 
+async function searchComicByTitle(URL, searchTerm) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
 
+    
+    const filteredComics = data.data.results.filter(comic =>
+      comic.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
+    displayComics(filteredComics);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
+function displayComics(comics) {
+  const appElement = document.getElementById('app');
+  appElement.innerHTML = '';
+  comics.forEach(comic => {
+    const comicElement = document.createElement('div');
+    comicElement.innerHTML = `
+      <h3>${comic.title}</h3>
+      <p>${comic.description}</p>
+      <p>${comic.urls}
+      <p>${comic.issueNumber}
+      <hr />
+    `;
+    appElement.appendChild(comicElement);
+  });
+}
+
+function displayComics(comics) {
+  const appElement = document.getElementById('app');
+  appElement.innerHTML = '';
+
+  
+  comics.forEach(comic => {
+    const comicElement = document.createElement('div');
+    comicElement.innerHTML = `
+      <h3>${comic.title}</h3>
+      <img src="${getComicImageSrc(comic)}" alt="${comic.title}" />
+      
+      <p> Issue Number : ${comic.issueNumber}
+      <hr />
+    `;
+    appElement.appendChild(comicElement);
+  });
+}
+
+function getComicImageSrc(comic) {
+  return `${comic.thumbnail.path}.${comic.thumbnail.extension}`;
+}
 
 
